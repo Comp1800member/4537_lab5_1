fa0f77d31daa31ca5e97adec5d268f619ea6fba9
DESKTOP-RJMDTP3\owner <Jonathanliu865@gmail.com>
2023-10-19 17:08:24 -0700

    procfile

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function insertRows() {
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", `https://patients-d127ea17110d.herokuapp.com/api/insertRows`);
+    xhr.open("POST", `localhost:8888/api/insertRows`);
     xhr.onload = function () {
         document.getElementById("insertRowsResponse").innerHTML = xhr.responseText;
     };
@@ -11,7 +11,7 @@ function executeQuery() {
     const query = document.getElementById("query").value.trim();
     if (query.startsWith("SELECT") || query.startsWith("INSERT")) {
         const xhr = new XMLHttpRequest();
-        xhr.open("POST", `https://patients-d127ea17110d.herokuapp.com/api/query`);
+        xhr.open("POST", `localhost:8888/api/query`);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onload = function () {
             document.getElementById("executeQueryResponse").innerHTML = xhr.responseText;
